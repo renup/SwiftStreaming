cacbbc084298c174e7889238e8b626adf3f5a093
Renu Punjabi <rpunjabi12@gmail.com>
2017-12-27 17:28:13 -0800

    added streaming logic in networkManager class

--- SwiftStreaming/Model Files/NetworkManager.swift
+++ SwiftStreaming/Model Files/NetworkManager.swift
@@ -10,5 +10,65 @@ import Foundation
 
 class NetworkManager : NSObject, URLSessionDataDelegate {
 
+    static var shared = NetworkManager()
+    private var session: URLSession! = nil
+    private var streamingTask: URLSessionDataTask? = nil
+    var isStreaming: Bool { return self.streamingTask != nil }
+    var outputStream: OutputStream? = nil
+    
+    override init() {
+        super.init()
+        let config = URLSessionConfiguration.default
+        
+        config.requestCachePolicy = .reloadIgnoringLocalCacheData
+        
+        self.session = URLSession(configuration: config, delegate: self, delegateQueue: .main)
+    }
+    
+    func startStreaming() {
+        precondition( !self.isStreaming )
+        let url = URL(string: "https://hp-server-toy.herokuapp.com/?since=20171227")!
+        let request = URLRequest(url: url)//SAME
+        
+        let task = self.session.dataTask(with: request)
+        //uploadTask(withStreamedRequest: request)
+        
+        self.streamingTask = task
+        task.resume() //SAME
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        print("Now description")
+        
+        // NEW - try printing just data also if you like
+        if let responseText = String(data: data, encoding: .utf8) {
+            print(responseText)
+        }
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let error = error as NSError? {
+            print("task error: \(error.domain), \(error.code)")
+        } else {
+            print("task complete")
+        }
+    }
+    
+    func stopStreaming() {
+        guard let task = self.streamingTask else {
+            return
+        }
+        self.streamingTask = nil
+        task.cancel()
+        self.closeStream()
+    }
+    
+    
+    private func closeStream() {
+        if let stream = self.outputStream {
+            stream.close()
+            self.outputStream = nil
+        }
+    }
 
 }
--- SwiftStreaming/StreamingTableViewController.swift
+++ SwiftStreaming/StreamingTableViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class StreamingTableViewController: UITableViewController {
 
+    var dataSource: NSDictionary? {
+        didSet {
+           tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getStreamingData()
+    }
+    
+    func getStreamingData(){
+        if NetworkManager.shared.isStreaming {
+            NetworkManager.shared.stopStreaming()
+        } else {
+            NetworkManager.shared.startStreaming()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +35,13 @@ class StreamingTableViewController: UITableViewController {
     }
 
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource?.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
 

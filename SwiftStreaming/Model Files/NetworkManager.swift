25cf64148ef21f2bc3229f0823eb2e7bbcdf360d
Renu Punjabi <rpunjabi12@gmail.com>
2017-12-27 17:09:01 -0800

    Added NetworkManager class

---
+++ SwiftStreaming/Model Files/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  SwiftStreaming
+//
+//  Created by Renu Punjabi on 12/27/17.
+//  Copyright © 2017 Renu Punjabi. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager : NSObject, URLSessionDataDelegate {
+
+
+}
